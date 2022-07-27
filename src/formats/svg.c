bb535b22a1b545aa193576fbb9c03825bcdfed64
Artem Senichev <artemsen@gmail.com>
2022-07-27 19:04:58 +0300

    svg: Render into image buffer
    Uses image buffer instead of creating new one in cairo.
    
    Signed-off-by: Artem Senichev <artemsen@gmail.com>

--- src/formats/svg.c
+++ src/formats/svg.c
@@ -9,31 +9,64 @@
 #include <string.h>
 
 // SVG uses physical units to store size,
-// these macro defines default viewport dimension in pixels
-#define VIEWPORT_SIZE 2048
+// these macro defines default viewbox dimension in pixels
+#define RENDER_SIZE 1024
 
-// SVG signature
-static const uint8_t signature[] = { '<' };
+// Max offset of the root svg node in xml file
+#define MAX_OFFSET 1024
+
+/**
+ * Check if data is SVG.
+ * @param data raw image data
+ * @param size size of image data in bytes
+ * @return true if it is an SVG format
+ */
+static bool is_svg(const uint8_t* data, size_t size)
+{
+    const char svg_begin[] = "<svg";
+    const char xml_begin[] = "<?xml";
+
+    // svg is an xml, skip spaces from the start
+    while (size && isspace(*data) != 0) {
+        ++data;
+        --size;
+    }
+
+    if (size > sizeof(svg_begin) &&
+        strncmp((const char*)data, svg_begin, sizeof(svg_begin) - 1) == 0) {
+        return true;
+    }
+
+    if (size > sizeof(xml_begin) &&
+        strncmp((const char*)data, xml_begin, sizeof(xml_begin) - 1) == 0) {
+        // search for svg node
+        size_t pos = sizeof(xml_begin);
+        while (pos < MAX_OFFSET && pos + sizeof(svg_begin) < size) {
+            if (strncmp((const char*)&data[pos], svg_begin,
+                        sizeof(svg_begin) - 1) == 0) {
+                return true;
+            }
+            ++pos;
+        }
+    }
+
+    return false;
+}
 
 // SVG loader implementation
 enum loader_status decode_svg(struct image* ctx, const uint8_t* data,
                               size_t size)
 {
     RsvgHandle* svg;
+    gboolean has_vb_real;
+    RsvgRectangle vb_real;
+    RsvgRectangle vb_render;
     GError* err = NULL;
-    gboolean has_viewport;
-    RsvgRectangle viewport;
     cairo_surface_t* surface = NULL;
     cairo_t* cr = NULL;
     cairo_status_t status;
 
-    // check signature, this an xml, so skip spaces from the start
-    while (size && isspace(*data) != 0) {
-        ++data;
-        --size;
-    }
-    if (size < sizeof(signature) ||
-        memcmp(data, signature, sizeof(signature))) {
+    if (!is_svg(data, size)) {
         return ldr_unsupported;
     }
 
@@ -46,56 +79,64 @@ enum loader_status decode_svg(struct image* ctx, const uint8_t* data,
 
     // define image size in pixels
     rsvg_handle_get_intrinsic_dimensions(svg, NULL, NULL, NULL, NULL,
-                                         &has_viewport, &viewport);
-    if (!has_viewport) {
-        viewport.x = 0;
-        viewport.y = 0;
-        viewport.width = VIEWPORT_SIZE;
-        viewport.height = VIEWPORT_SIZE;
+                                         &has_vb_real, &vb_real);
+    vb_render.x = 0;
+    vb_render.y = 0;
+    if (has_vb_real) {
+        if (vb_real.width < vb_real.height) {
+            vb_render.width = RENDER_SIZE * (vb_real.width / vb_real.height);
+            vb_render.height = RENDER_SIZE;
+        } else {
+            vb_render.width = RENDER_SIZE;
+            vb_render.height = RENDER_SIZE * (vb_real.height / vb_real.width);
+        }
+    } else {
+        vb_render.width = RENDER_SIZE;
+        vb_render.height = RENDER_SIZE;
     }
 
-    // prepare surface and metadata
-    surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, viewport.width,
-                                         viewport.height);
+    // allocate and bind buffer
+    if (!image_allocate(ctx, vb_render.width, vb_render.height)) {
+        goto fail;
+    }
+    surface = cairo_image_surface_create_for_data(
+        (uint8_t*)ctx->data, CAIRO_FORMAT_ARGB32, ctx->width, ctx->height,
+        ctx->width * sizeof(ctx->data[0]));
     status = cairo_surface_status(surface);
     if (status != CAIRO_STATUS_SUCCESS) {
         const char* desc = cairo_status_to_string(status);
-        image_error(ctx, "unable to create svg surface: %s",
+        image_error(ctx, "unable to create cairo surface: %s",
                     desc ? desc : "unknown error");
-        if (surface) {
-            cairo_surface_destroy(surface);
-            surface = NULL;
-        }
-        g_object_unref(svg);
-        return ldr_fmterror;
+        goto fail;
     }
 
     // render svg to surface
     cr = cairo_create(surface);
-    if (!rsvg_handle_render_document(svg, cr, &viewport, &err)) {
+    if (!rsvg_handle_render_document(svg, cr, &vb_render, &err)) {
         image_error(ctx, "unable to decode svg: %s",
                     err && err->message ? err->message : "unknown error");
-        cairo_destroy(cr);
-        cairo_surface_destroy(surface);
-        g_object_unref(svg);
-        return ldr_fmterror;
-    }
-
-    if (!image_allocate(ctx, viewport.width, viewport.height)) {
-        cairo_destroy(cr);
-        cairo_surface_destroy(surface);
-        g_object_unref(svg);
-        return ldr_fmterror;
+        goto fail;
     }
 
     image_add_meta(ctx, "Format", "SVG");
+    image_add_meta(ctx, "Real size", "%0.2fx%0.2f", vb_real.width,
+                   vb_real.height);
     ctx->alpha = true;
-    memcpy((void*)ctx->data, cairo_image_surface_get_data(surface),
-           ctx->width * ctx->height * sizeof(argb_t));
 
     cairo_destroy(cr);
     cairo_surface_destroy(surface);
     g_object_unref(svg);
 
     return ldr_success;
+
+fail:
+    if (cr) {
+        cairo_destroy(cr);
+    }
+    if (surface) {
+        cairo_surface_destroy(surface);
+    }
+    image_deallocate(ctx);
+    g_object_unref(svg);
+    return ldr_fmterror;
 }
