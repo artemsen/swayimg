ce39a06b51bd97eed66279879613265bc7856613
Artem Senichev <artemsen@gmail.com>
2025-07-06 12:06:28 +0300

    drm: Fix CRTC initialization
    Signed-off-by: Artem Senichev <artemsen@gmail.com>

--- src/ui/drm.c
+++ src/ui/drm.c
@@ -44,19 +44,19 @@ struct drm {
 
 /**
  * Free frame buffer.
- * @param ctx DRM context pointer
+ * @param fd DRM file descriptor
  * @param fb pointer to the frame buffer description
  */
-static void free_fb(struct drm* ctx, struct fbuffer* fb)
+static void free_fb(int fd, struct fbuffer* fb)
 {
     if (fb->id) {
-        drmModeRmFB(ctx->fd, fb->id);
+        drmModeRmFB(fd, fb->id);
     }
     if (fb->handle) {
         struct drm_mode_destroy_dumb destroy = {
             .handle = fb->handle,
         };
-        drmIoctl(ctx->fd, DRM_IOCTL_MODE_DESTROY_DUMB, &destroy);
+        drmIoctl(fd, DRM_IOCTL_MODE_DESTROY_DUMB, &destroy);
     }
     if (fb->data) {
         munmap(fb->data, fb->size);
@@ -65,13 +65,12 @@ static void free_fb(struct drm* ctx, struct fbuffer* fb)
 
 /**
  * Create frame buffer.
- * @param ctx DRM context pointer
+ * @param fd DRM file descriptor
  * @param fb pointer to the frame buffer description
  * @param width,height size of frame buffer in pixels
  * @return true if frame buffer was created successfully
  */
-static bool create_fb(struct drm* ctx, struct fbuffer* fb, size_t width,
-                      size_t height)
+static bool create_fb(int fd, struct fbuffer* fb, size_t width, size_t height)
 {
     struct drm_mode_create_dumb dumb_create = { 0 };
     struct drm_mode_map_dumb dumb_map = { 0 };
@@ -83,7 +82,7 @@ static bool create_fb(struct drm* ctx, struct fbuffer* fb, size_t width,
     dumb_create.width = width;
     dumb_create.height = height;
     dumb_create.bpp = 32; // xrgb
-    if (drmIoctl(ctx->fd, DRM_IOCTL_MODE_CREATE_DUMB, &dumb_create) < 0) {
+    if (drmIoctl(fd, DRM_IOCTL_MODE_CREATE_DUMB, &dumb_create) < 0) {
         const int rc = errno;
         fprintf(stderr, "Unable to create dumb: [%d] %s\n", rc, strerror(rc));
         goto fail;
@@ -95,7 +94,7 @@ static bool create_fb(struct drm* ctx, struct fbuffer* fb, size_t width,
     // create frambuffer
     handles[0] = dumb_create.handle;
     strides[0] = dumb_create.pitch;
-    if (drmModeAddFB2(ctx->fd, dumb_create.width, dumb_create.height,
+    if (drmModeAddFB2(fd, dumb_create.width, dumb_create.height,
                       DRM_FORMAT_XRGB8888, handles, strides, offsets, &fb->id,
                       0) < 0) {
         const int rc = errno;
@@ -106,7 +105,7 @@ static bool create_fb(struct drm* ctx, struct fbuffer* fb, size_t width,
 
     // map frambuffer
     dumb_map.handle = dumb_create.handle;
-    if (drmIoctl(ctx->fd, DRM_IOCTL_MODE_MAP_DUMB, &dumb_map) < 0) {
+    if (drmIoctl(fd, DRM_IOCTL_MODE_MAP_DUMB, &dumb_map) < 0) {
         const int rc = errno;
         fprintf(stderr, "Unable to map frambuffer: [%d] %s\n", rc,
                 strerror(rc));
@@ -114,8 +113,8 @@ static bool create_fb(struct drm* ctx, struct fbuffer* fb, size_t width,
     }
 
     // create memory map
-    fb->data = mmap(0, dumb_create.size, PROT_READ | PROT_WRITE, MAP_SHARED,
-                    ctx->fd, dumb_map.offset);
+    fb->data = mmap(0, dumb_create.size, PROT_READ | PROT_WRITE, MAP_SHARED, fd,
+                    dumb_map.offset);
     if (fb->data == MAP_FAILED) {
         const int rc = errno;
         fprintf(stderr, "Unable to create mmap: [%d] %s\n", rc, strerror(rc));
@@ -126,20 +125,47 @@ static bool create_fb(struct drm* ctx, struct fbuffer* fb, size_t width,
     return true;
 
 fail:
-    free_fb(ctx, fb);
+    free_fb(fd, fb);
     memset(fb, 0, sizeof(*fb));
     return false;
 }
 
 /**
- * Get first suitable connector.
- * @param display pointer to the display context
- * @param mode output mode
- * @return true if connector found
+ * Get CRTC id.
+ * @param fd DRM file descriptor
+ * @param res DRM resources
+ * @param conn DRM connector
+ * @return CRTC id or 0 if not found
  */
-static bool get_connector(struct drm* display, drmModeModeInfo* mode)
+static uint32_t get_crtc(int fd, drmModeRes* res, drmModeConnector* conn)
 {
-    drmModeRes* res = drmModeGetResources(display->fd);
+    for (int i = 0; i < conn->count_encoders; ++i) {
+        drmModeEncoder* enc = drmModeGetEncoder(fd, conn->encoders[i]);
+        if (enc) {
+            for (int i = 0; i < res->count_crtcs; ++i) {
+                if (enc->possible_crtcs & (1 << i)) {
+                    drmModeFreeEncoder(enc);
+                    return res->crtcs[i];
+                }
+            }
+            drmModeFreeEncoder(enc);
+        }
+    }
+    return 0;
+}
+
+/**
+ * Initialize DRM connection.
+ * @param fd DRM file descriptor
+ * @param conn_id output connector id
+ * @param crtc_id output CRTC id
+ * @param mode output mode
+ * @return true DRM initialized
+ */
+static bool drm_init(int fd, uint32_t* conn_id, uint32_t* crtc_id,
+                     drmModeModeInfo* mode)
+{
+    drmModeRes* res = drmModeGetResources(fd);
     if (!res) {
         const int rc = errno;
         fprintf(stderr, "Unable to get DRM modes: [%d] %s\n", rc, strerror(rc));
@@ -148,8 +174,10 @@ static bool get_connector(struct drm* display, drmModeModeInfo* mode)
 
     for (int i = 0; i < res->count_connectors; ++i) {
         drmModeConnector* conn;
+        drmModeEncoder* enc = NULL;
 
-        conn = drmModeGetConnector(display->fd, res->connectors[i]);
+        // open next connector
+        conn = drmModeGetConnector(fd, res->connectors[i]);
         if (!conn) {
             continue;
         }
@@ -158,22 +186,31 @@ static bool get_connector(struct drm* display, drmModeModeInfo* mode)
             continue;
         }
 
-        for (int j = 0; j < conn->count_encoders; ++j) {
-            drmModeEncoder* enc;
-            enc = drmModeGetEncoder(display->fd, conn->encoders[j]);
-            if (!enc) {
-                continue;
-            }
-            // just get first available
-            *mode = conn->modes[0];
-            display->crtc_id = enc->crtc_id;
-            display->conn_id = conn->connector_id;
-            drmModeFreeEncoder(enc);
-            drmModeFreeConnector(conn);
-            drmModeFreeResources(res);
-            return true;
+        // get first available encoder
+        for (int j = 0; !enc && j < conn->count_encoders; ++j) {
+            enc = drmModeGetEncoder(fd, conn->encoders[j]);
         }
+        if (!enc) {
+            drmModeFreeConnector(conn);
+            continue;
+        }
+
+        // get preferred mode
+        *mode = conn->modes[0]; // use first available as fallback
+        for (int j = 0; j < conn->count_modes; ++j) {
+            if (conn->modes[j].flags & DRM_MODE_TYPE_PREFERRED) {
+                *mode = conn->modes[0];
+                break;
+            }
+        }
+
+        *conn_id = conn->connector_id;
+        *crtc_id = get_crtc(fd, res, conn);
+
+        drmModeFreeEncoder(enc);
         drmModeFreeConnector(conn);
+        drmModeFreeResources(res);
+        return true;
     }
 
     drmModeFreeResources(res);
@@ -220,8 +257,8 @@ static void drm_free(void* data)
         drmModeFreeCrtc(crtc);
     }
 
-    free_fb(ctx, &ctx->fb[0]);
-    free_fb(ctx, &ctx->fb[1]);
+    free_fb(ctx->fd, &ctx->fb[0]);
+    free_fb(ctx->fd, &ctx->fb[1]);
 
     if (ctx->fd != -1) {
         close(ctx->fd);
@@ -264,12 +301,12 @@ void* ui_init_drm(struct ui* handlers)
         goto fail;
     }
 
-    if (!get_connector(ctx, &mode)) {
+    if (!drm_init(ctx->fd, &ctx->conn_id, &ctx->crtc_id, &mode)) {
         goto fail;
     }
 
-    if (!create_fb(ctx, &ctx->fb[0], mode.hdisplay, mode.vdisplay) ||
-        !create_fb(ctx, &ctx->fb[1], mode.hdisplay, mode.vdisplay)) {
+    if (!create_fb(ctx->fd, &ctx->fb[0], mode.hdisplay, mode.vdisplay) ||
+        !create_fb(ctx->fd, &ctx->fb[1], mode.hdisplay, mode.vdisplay)) {
         goto fail;
     }
     ctx->cfb = &ctx->fb[0];
