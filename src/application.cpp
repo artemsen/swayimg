artemsen/swayimg

commit 19afb7f60f7f123037600c09381b301015097cbf

lua: Add support for batch add/remove images
Allows to add and remove multiple image entries at once.

Signed-off-by: Artem Senichev <artemsen@gmail.com>

src/application.cpp
@@ -143,28 +143,41 @@ AppMode* Application::current_mode()
     return nullptr;
 }
 
-ImageEntryPtr Application::add_image_entry(const std::filesystem::path& path)
+ImageEntryPtr
+Application::add_images(const std::vector<std::filesystem::path>& paths)
 {
-    const std::list<ImageEntryPtr> entries = ImageList::self().add(path);
+    ImageList& il = ImageList::self();
+
+    std::list<ImageEntryPtr> entries;
+    for (const auto& path : paths) {
+        entries.splice(entries.end(), il.add(path));
+    }
+
     if (!entries.empty()) {
-        AppMode* mode = current_mode();
-        for (auto& it : entries) {
-            mode->handle_imagelist(AppMode::ImageListEvent::Create, it);
-        }
+        current_mode()->handle_imagelist(AppMode::ImageListEvent::Create,
+                                         entries);
         redraw();
         return entries.front();
     }
+
     return nullptr;
 }
 
-void Application::remove_image_entry(const std::filesystem::path& path)
+void Application::remove_images(const std::vector<std::filesystem::path>& paths)
 {
     ImageList& il = ImageList::self();
-    const ImageEntryPtr entry = il.find(path);
-    if (entry) {
-        il.remove(entry);
-        AppMode* mode = current_mode();
-        mode->handle_imagelist(AppMode::ImageListEvent::Remove, entry);
+
+    std::list<ImageEntryPtr> entries;
+    for (const auto& path : paths) {
+        const ImageEntryPtr entry = il.find(path);
+        if (entry) {
+            entries.push_back(entry);
+        }
+    }
+
+    if (!entries.empty()) {
+        current_mode()->handle_imagelist(AppMode::ImageListEvent::Remove,
+                                         entries);
         redraw();
     }
 }
@@ -489,7 +502,7 @@ void Application::handle_event(const AppEvent::FileCreate& event)
 {
     const ImageList& il = ImageList::self();
     if (!std::filesystem::is_directory(event.path) || il.recursive) {
-        add_image_entry(event.path);
+        add_images({ event.path });
     }
 }
 
@@ -500,7 +513,7 @@ void Application::handle_event(const AppEvent::FileModify& event)
         const ImageEntryPtr entry = il.find(event.path);
         if (entry) {
             current_mode()->handle_imagelist(AppMode::ImageListEvent::Modify,
-                                             entry);
+                                             { entry });
         }
     }
 }
@@ -509,7 +522,7 @@ void Application::handle_event(const AppEvent::FileRemove& event)
 {
     // ignore directories, files will be removed as standalone event
     if (!std::filesystem::is_directory(event.path)) {
-        remove_image_entry(event.path);
+        remove_images({ event.path });
     }
 }
 

extra/swayimg.lua
@@ -284,15 +284,15 @@ function swayimg.imagelist.size() end
 ---@return swayimg.entry[] # Array with all file entries
 function swayimg.imagelist.get() end
 
----Add entry to the image list.
+---Add entries to the image list.
 ---Since 5.0.
----@param path string Path to add
-function swayimg.imagelist.add(path) end
+---@param paths string|string[] Paths to add
+function swayimg.imagelist.add(paths) end
 
----Remove entry from the image list.
+---Remove entries from the image list.
 ---Since 5.0.
----@param path string Path to remove
-function swayimg.imagelist.remove(path) end
+---@param paths string|string[] Paths to remove
+function swayimg.imagelist.remove(paths) end
 
 ---Set sort order of the image list.
 ---Since 5.0.

src/application.hpp
@@ -81,17 +81,17 @@ public:
     AppMode* current_mode();
 
     /**
-     * Add entry to image list.
-     * @param path image path
-     * @return added entry (first one for directory)
+     * Add entries to the image list.
+     * @param paths list of image paths to add
+     * @return first added entry
      */
-    ImageEntryPtr add_image_entry(const std::filesystem::path& path);
+    ImageEntryPtr add_images(const std::vector<std::filesystem::path>& paths);
 
     /**
-     * Remove entry from image list.
-     * @param path image path
+     * Remove entries from the image list.
+     * @param paths list of image paths to remove
      */
-    void remove_image_entry(const std::filesystem::path& path);
+    void remove_images(const std::vector<std::filesystem::path>& paths);
 
     /**
      * Add file descriptor to monitor.

src/appmode.cpp
@@ -53,7 +53,8 @@ bool AppMode::handle_signal(const InputSignal& input)
     return false;
 }
 
-void AppMode::handle_imagelist(const ImageListEvent, const ImageEntryPtr&)
+void AppMode::handle_imagelist(const ImageListEvent,
+                               const std::list<ImageEntryPtr>&)
 {
     Text& text = Text::self();
     text.set_field(Text::FIELD_LIST_INDEX,

src/appmode.hpp
@@ -10,6 +10,7 @@
 
 #include <array>
 #include <functional>
+#include <list>
 #include <map>
 #include <vector>
 
@@ -104,10 +105,10 @@ public:
     /**
      * Handle image list changes.
      * @param event event type
-     * @param entry updated image entry
+     * @param entries list of added/removed/updated image entries
      */
     virtual void handle_imagelist(const ImageListEvent event,
-                                  const ImageEntryPtr& entry);
+                                  const std::list<ImageEntryPtr>& entries);
 
     /**
      * Check if current mode is active.

src/gallery.cpp
@@ -377,24 +377,32 @@ void Gallery::handle_pinch(const double scale_delta)
 }
 
 void Gallery::handle_imagelist(const ImageListEvent event,
-                               const ImageEntryPtr& entry)
+                               const std::list<ImageEntryPtr>& entries)
 {
-    AppMode::handle_imagelist(event, entry);
+    AppMode::handle_imagelist(event, entries);
 
     if (event == ImageListEvent::Modify || event == ImageListEvent::Remove) {
         // remove entry from cache
         const std::scoped_lock lock(mutex);
-        cache.erase(entry);
+        for (const auto& entry : entries) {
+            cache.erase(entry);
+        }
     }
 
-    if (event == ImageListEvent::Remove && entry == layout.get_selected()) {
-        if (!layout.select(Layout::Right) && !layout.select(Layout::Left)) {
-            Log::info("No more images to view, exit");
-            Application::self().exit(0);
-            return;
+    if (event == ImageListEvent::Remove) {
+        for (const auto& entry : entries) {
+            if (entry == layout.get_selected()) {
+                if (!layout.select(Layout::Right) &&
+                    !layout.select(Layout::Left)) {
+                    Log::info("No more images to view, exit");
+                    Application::self().exit(0);
+                    return;
+                }
+                switch_current();
+            }
         }
-        switch_current();
     }
+
     layout.update();
     Application::redraw();
 }

src/gallery.hpp
@@ -152,7 +152,7 @@ public:
                       const Point& delta) override;
     void handle_pinch(const double scale_delta) override;
     void handle_imagelist(const ImageListEvent event,
-                          const ImageEntryPtr& entry) override;
+                          const std::list<ImageEntryPtr>& entries) override;
 
 private:
     /**

src/luaengine.cpp
@@ -518,12 +518,36 @@ void LuaEngine::bind_imagelist_api()
                          return table;
                      })
         .addFunction("add",
-                     [](const std::string& path) {
-                         Application::self().add_image_entry(path);
+                     [this](const luabridge::LuaRef& val) {
+                         std::vector<std::filesystem::path> paths;
+                         if (val.isString()) {
+                             paths.emplace_back(val.tostring());
+                         } else if (val.isTable()) {
+                             const size_t arr_sz = val.length();
+                             paths.reserve(arr_sz);
+                             for (size_t i = 1; i <= arr_sz; ++i) {
+                                 paths.emplace_back(val[i].tostring());
+                             }
+                         } else {
+                             raise_error("Invalid argument type");
+                         }
+                         Application::self().add_images(paths);
                      })
         .addFunction("remove",
-                     [](const std::string& path) {
-                         Application::self().remove_image_entry(path);
+                     [this](const luabridge::LuaRef& val) {
+                         std::vector<std::filesystem::path> paths;
+                         if (val.isString()) {
+                             paths.emplace_back(val.tostring());
+                         } else if (val.isTable()) {
+                             const size_t arr_sz = val.length();
+                             paths.reserve(arr_sz);
+                             for (size_t i = 1; i <= arr_sz; ++i) {
+                                 paths.emplace_back(val[i].tostring());
+                             }
+                         } else {
+                             raise_error("Invalid argument type");
+                         }
+                         Application::self().remove_images(paths);
                      })
         .addFunction("set_order",
                      [this](const std::string& name) {
@@ -651,7 +675,7 @@ void LuaEngine::bind_viewer_api(const char* name)
                      [ensure_active, mode](const std::string& path) {
                          ensure_active("open");
                          ImageEntryPtr entry =
-                             Application::self().add_image_entry(path);
+                             Application::self().add_images({ path });
                          if (!entry) {
                              // try to get existing entry
                              entry = ImageList::self().find(path);

commit 8aceca5e825b8b866b6583954527f43e57f5e6cc

imagelist: Fix removing entries
Regression from 19afb7f.
Resolves #504.

Signed-off-by: Artem Senichev <artemsen@gmail.com>

src/application.cpp
@@ -172,6 +172,7 @@ void Application::remove_images(const std::vector<std::filesystem::path>& paths)
         const ImageEntryPtr entry = il.find(path);
         if (entry) {
             entries.push_back(entry);
+            il.remove(entry);
         }
     }
 