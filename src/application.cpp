artemsen/swayimg

commit d9810085e831d91c9d4ea403ba3917c8055a1313

imagelist: Use bulk load for all entry adding
Signed-off-by: litoj <invisiblemancz@gmail.com>

src/application.cpp
@@ -152,10 +152,7 @@ Application::add_images(const std::vector<std::filesystem::path>& paths)
 {
     ImageList& il = ImageList::self();
 
-    std::list<ImageEntryPtr> entries;
-    for (const auto& path : paths) {
-        entries.splice(entries.end(), il.add(path));
-    }
+    const std::list<ImageEntryPtr> entries = il.add(paths);
 
     if (!entries.empty()) {
         current_mode()->handle_imagelist(AppMode::ImageListEvent::Create,
@@ -246,6 +243,10 @@ ImageEntryPtr Application::il_initialize()
     if (Log::verbose_enable()) {
         Log::verbose("Image list loaded in {:.6f} sec", timer.time());
     }
+
+    // disable loading of adjacent files, otherwise fs mon will add unnecessary
+    // files to the list on every call after startup
+    il.adjacent = false;
 
     return first_entry;
 }

src/imagelist.cpp
@@ -179,17 +179,7 @@ ImageList::add(const std::vector<std::filesystem::path>& sources)
         sort();
     }
 
-    // disable loading of adjacent files, otherwise fs mon will add unnecessary
-    // files to the list on every call after startup
-    adjacent = false;
-
     return added;
-}
-
-std::list<ImageEntryPtr> ImageList::add(const std::filesystem::path& path)
-{
-    const std::scoped_lock lock(mutex);
-    return add(path, true);
 }
 
 ImageEntryPtr ImageList::remove(const ImageEntryPtr& entry, const bool forward)
@@ -412,6 +402,9 @@ std::list<ImageEntryPtr> ImageList::add(const std::filesystem::path& path,
         }
 
         if (!adjacent) {
+            if (fsmon) {
+                FsMonitor::self().add(abs_path);
+            }
             return added;
         }
         abs_path = abs_path.parent_path();
@@ -490,9 +483,6 @@ ImageEntryPtr ImageList::add_file(const std::filesystem::path& path,
     entry->size = std::filesystem::file_size(path);
     if (!add_entry(entry, ordered)) {
         return nullptr;
-    }
-    if (fsmon) {
-        FsMonitor::self().add(path);
     }
     return entry;
 }

src/imagelist.hpp
@@ -43,14 +43,6 @@ public:
     static ImageList& self();
 
     /**
-     * Load image list from specified sources.
-     * @param sources list of sources to load
-     * @return list of added entries in source order
-     */
-    std::list<ImageEntryPtr>
-    add(const std::vector<std::filesystem::path>& sources);
-
-    /**
      * Load image list from text file.
      * @param list_file path to the file to load
      * @return first entry from source list
@@ -58,11 +50,12 @@ public:
     ImageEntryPtr load(const std::filesystem::path& list_file);
 
     /**
-     * Add file or special source to the list.
-     * @param path path to the file or special source
-     * @return list of added entries
+     * Add all images in specified sources.
+     * @param sources list of sources to load
+     * @return list of added entries in source order
      */
-    std::list<ImageEntryPtr> add(const std::filesystem::path& path);
+    std::list<ImageEntryPtr>
+    add(const std::vector<std::filesystem::path>& sources);
 
     /**
      * Remove image entry from the list.

test/imagelist_test.cpp
@@ -134,7 +134,7 @@ TEST(ImageListTest, LoadFile)
 TEST(ImageListTest, AddDir)
 {
     ImageList il;
-    il.add(TEST_DATA_DIR);
+    il.add({ TEST_DATA_DIR });
     ASSERT_NE(il.size(), 0UL);
 }
 
@@ -371,7 +371,7 @@ TEST(ImageListTest, SortRandom)
     // clang-format on
 
     for (auto& it : paths) {
-        il.add(it);
+        il.add({ it });
     }
 
     ImageEntryPtr entry = il.get(nullptr, ImageList::Dir::First);
@@ -393,8 +393,8 @@ TEST(ImageListTest, GetFirstLast)
     ASSERT_FALSE(il.get(nullptr, ImageList::Dir::First));
     ASSERT_FALSE(il.get(nullptr, ImageList::Dir::Last));
 
-    il.add("exec://first");
-    il.add("exec://last");
+    il.add({ "exec://first" });
+    il.add({ "exec://last" });
 
     ASSERT_EQ(il.size(), 2UL);
 
@@ -411,8 +411,8 @@ TEST(ImageListTest, GetNextPrev)
 {
     ImageList il;
 
-    il.add("exec://first");
-    il.add("exec://last");
+    il.add({ "exec://first" });
+    il.add({ "exec://last" });
 
     ImageEntryPtr entry = il.get(nullptr, ImageList::Dir::First);
     ASSERT_TRUE(entry);

test/layout_test.cpp
@@ -36,7 +36,7 @@ protected:
     {
         ImageList& il = ImageList::self();
         for (size_t i = 0; i < total; ++i) {
-            il.add(std::string(ImageEntry::SRC_EXEC) + std::to_string(i));
+            il.add({ ImageEntry::SRC_EXEC + std::to_string(i) });
         }
         layout.set_window_size({ 80, 60 });
         layout.set_thumb_size(10);

commit d2c011b208d9c19bbdd78371477ee7bcb9b83041

Fix segfault on empty list
Regression from #505.

Signed-off-by: Artem Senichev <artemsen@gmail.com>

src/application.cpp
@@ -227,17 +227,19 @@ ImageEntryPtr Application::il_initialize()
         first_entry = il.load(sparams.from_file);
     }
     if (!sparams.sources.empty()) {
+        std::list<ImageEntryPtr> added;
         if (sparams.sources.size() == 1 && sparams.sources[0] == "-") {
-            first_entry = il.add(std::vector<std::filesystem::path> {
-                                     ImageEntry::SRC_STDIN })
-                              .front();
+            added = il.add(
+                std::vector<std::filesystem::path> { ImageEntry::SRC_STDIN });
         } else {
-            first_entry = il.add(sparams.sources).front();
+            added = il.add(sparams.sources);
         }
+        first_entry = added.empty() ? nullptr : added.front();
     }
     if (sparams.from_file.empty() && sparams.sources.empty()) {
-        first_entry =
-            il.add(std::vector<std::filesystem::path> { "." }).front();
+        const std::list<ImageEntryPtr> added =
+            il.add(std::vector<std::filesystem::path> { "." });
+        first_entry = added.empty() ? nullptr : added.front();
     }
 
     if (Log::verbose_enable()) {