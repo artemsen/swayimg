artemsen/swayimg

commit 1d76a11d6d3a7948c401e7c032b8b479930fc657

Fix stopping key repeat on old key release
Signed-off-by: Krzysztof Wolski <kwwol@protonmail.com>

src/ui_wayland.cpp
@@ -35,7 +35,7 @@ public:
                                   struct wl_surface*)
     {
         const UiWayland* ui = reinterpret_cast<UiWayland*>(data);
-        ui->xkb.stop_repeat();
+        ui->xkb.stop_repeat(XKB_KEY_NoSymbol);
     }
 
     static void on_keyboard_modifiers(void* data, struct wl_keyboard*, uint32_t,
@@ -68,11 +68,11 @@ public:
     {
         UiWayland* ui = reinterpret_cast<UiWayland*>(data);
 
+        key += 8;
+        const xkb_keysym_t keysym = ui->xkb.get_keysym(key);
         if (state == WL_KEYBOARD_KEY_STATE_RELEASED) {
-            ui->xkb.stop_repeat();
+            ui->xkb.stop_repeat(keysym);
         } else if (state == WL_KEYBOARD_KEY_STATE_PRESSED) {
-            key += 8;
-            const xkb_keysym_t keysym = ui->xkb.get_keysym(key);
             if (keysym != XKB_KEY_NoSymbol) {
                 Application::self().add_event(
                     AppEvent::KeyPress { keysym, ui->xkb.get_modifiers() });

src/xkb.cpp
@@ -67,9 +67,11 @@ void Xkb::start_repeat(const xkb_keycode_t code)
     }
 }
 
-void Xkb::stop_repeat() const
+void Xkb::stop_repeat(const xkb_keycode_t key) const
 {
-    repeat_timer.reset(0, 0);
+    if (key == XKB_KEY_NoSymbol || key == repeat_key) {
+        repeat_timer.reset(0, 0);
+    }
 }
 
 std::tuple<xkb_keysym_t, size_t> Xkb::get_repeat() const

src/xkb.hpp
@@ -46,8 +46,9 @@ public:
 
     /**
      * Stop repeat mode.
+     * @param key key to stop repeat or XKB_KEY_NoSymbol to stop unconditionally
      */
-    void stop_repeat() const;
+    void stop_repeat(const xkb_keycode_t key) const;
 
     /**
      * Get repeat description.

commit ef821e5f3535c4d6a62a0e6c63745a814c098a92

Fix repeat key handling
Regression from the commit 1d76a11d.

Resolves #566.

Signed-off-by: Artem Senichev <artemsen@gmail.com>

src/ui_wayland.cpp
@@ -35,7 +35,7 @@ public:
                                   struct wl_surface*)
     {
         const UiWayland* ui = reinterpret_cast<UiWayland*>(data);
-        ui->xkb.stop_repeat(XKB_KEY_NoSymbol);
+        ui->xkb.stop_repeat(0);
     }
 
     static void on_keyboard_modifiers(void* data, struct wl_keyboard*, uint32_t,
@@ -69,10 +69,10 @@ public:
         UiWayland* ui = reinterpret_cast<UiWayland*>(data);
 
         key += 8;
-        const xkb_keysym_t keysym = ui->xkb.get_keysym(key);
         if (state == WL_KEYBOARD_KEY_STATE_RELEASED) {
-            ui->xkb.stop_repeat(keysym);
+            ui->xkb.stop_repeat(key);
         } else if (state == WL_KEYBOARD_KEY_STATE_PRESSED) {
+            const xkb_keysym_t keysym = ui->xkb.get_keysym(key);
             if (keysym != XKB_KEY_NoSymbol) {
                 Application::self().add_event(
                     AppEvent::KeyPress { keysym, ui->xkb.get_modifiers() });

src/xkb.cpp
@@ -61,16 +61,14 @@ void Xkb::start_repeat(const xkb_keycode_t code)
 {
     if (repeat_rate && xkb_keymap_key_repeats(keymap, code)) {
         // start key repeat timer
-        // Shift won't capitalize the keys anymore.
-        repeat_key =
-            xkb_keysym_to_lower(xkb_state_key_get_one_sym(state, code));
+        repeat_code = code;
         repeat_timer.reset(repeat_delay, 1000 / repeat_rate);
     }
 }
 
-void Xkb::stop_repeat(const xkb_keycode_t key) const
+void Xkb::stop_repeat(const xkb_keycode_t code) const
 {
-    if (key == XKB_KEY_NoSymbol || key == repeat_key) {
+    if (code == 0 || code == repeat_code) {
         repeat_timer.reset(0, 0);
     }
 }
@@ -84,6 +82,8 @@ std::tuple<xkb_keysym_t, size_t> Xkb::get_repeat() const
         ++count;
     }
 
+    const xkb_keysym_t repeat_key =
+        xkb_keysym_to_lower(xkb_state_key_get_one_sym(state, repeat_code));
     return std::make_tuple(repeat_key, count);
 }
 

src/xkb.hpp
@@ -46,9 +46,9 @@ public:
 
     /**
      * Stop repeat mode.
-     * @param key key to stop repeat or XKB_KEY_NoSymbol to stop unconditionally
+     * @param key key to stop repeat or 0 to stop unconditionally
      */
-    void stop_repeat(const xkb_keycode_t key) const;
+    void stop_repeat(const xkb_keycode_t code) const;
 
     /**
      * Get repeat description.
@@ -113,7 +113,7 @@ private:
     FdTimer repeat_timer;
     size_t repeat_rate = 0;
     size_t repeat_delay = 0;
-    xkb_keysym_t repeat_key = XKB_KEY_NoSymbol;
+    xkb_keycode_t repeat_code = 0;
 
     // X keyboard extension handles
     xkb_context* context = nullptr;